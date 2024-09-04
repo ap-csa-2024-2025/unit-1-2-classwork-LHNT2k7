251599c4b09523af60501fb5595a93d0f9a368f1
LHNT2k7 <ltri9573@gmail.com>
2024-09-04 10:05:54 -0700

    do an

--- problem1.java
+++ problem1.java
@@ -1,1 +1,9 @@
 // Create your class here; don't forget the main method
+public class problem1
+{
+    public static void main(String[] args)
+    {
+        String food = Reese
+        System.out.print("I like to eat " + food + " as well!");
+    }
+}
