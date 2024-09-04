304fb59644b9357270fdb8a2175e460dc7845883
LHNT2k7 <ltri9573@gmail.com>
2024-09-04 10:16:47 -0700

    ten

--- problem2.java
+++ problem2.java
@@ -1,1 +1,12 @@
 // Create your class here; don't forget the main method
+public class problem1
+{
+    public static void main(String[] args)
+    {
+        String name = "Sandy ";
+        String name1 = "David ";
+        String name2 = "Elaine ";
+        String name3 = "Jesse ";
+        System.out.print(name + name1 + name2 + name3);
+    }
+}
